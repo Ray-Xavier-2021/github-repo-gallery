1528eb9584f301069602887bd4fcaedd60c2a532
Ray-Xavier-2021 <rayxavier.frontenddev.2021@gmail.com>
2021-08-04 10:40:48 -0400

    Added two global variables, a click event for list, a function to grab specific repo,array for languages

--- js/script.js
+++ js/script.js
@@ -2,11 +2,14 @@
 
 // 1. Create Global Variables
 //Displays profile information
-const profileInfo = document.querySelector('.overview');
+const profileInfoOverview = document.querySelector('.overview');
 //Username
 const username = 'Ray-Xavier-2021';
 // 1. Select the Repos List
 const repoList = document.querySelector('.repo-list');
+// 7a. Create two global variables for displaying repo info
+const reposArea = document.querySelector('.repos');
+const reposDataArea = document.querySelector('.repo-data');
 
 // 2. Fetch API JSON Data
 // 2a. Create async function to fetch info from GitHub profile
@@ -15,12 +18,12 @@ const getUserInfo = async function () {
     const response = await fetch(`https://api.github.com/users/${username}`);
 // 2c. Formats info retrieved into json object    
     const data = await response.json();
-    //console.log(data);
+    console.log(data);
 // 4. Call the Display Function & View Your Project    
 // 4a. Calls the function to display profile info
 displayUserInfo(data);    
 };
-// 2d. Calls the function
+// 2d. Calls the get user info function
 getUserInfo();
 
 // 3. Fetch & Display User Information
@@ -31,35 +34,36 @@ const displayUserInfo = function (data) {
 // 3c. Give the div a class name    
     div.classList.add('user-info');
 // 3d. Span innerHTML with the following elements: figure, img, p
-div.innerHTML = 
+    div.innerHTML = 
 // 3e. Display info with temp literals and relevant key.value properties using dot notation
-`
-<figure>
-<img alt="user avatar" src=${data.avatar_url} />
-</figure>
-<div>
-<p><strong>Name:</strong> ${data.name}</p
-><p><strong>Bio:</strong> ${data.bio}</p>
-<p><strong>Location:</strong> ${data.location}</p>
-<p><strong>Number of public repos:</strong> ${data.public_repos}</p>
-</div>`;    
-profileInfo.append(div);
-//Displays repos
-getUserRepo();
+    `
+    <figure>
+    <img alt="user avatar" src=${data.avatar_url} />
+    </figure>
+    <div>
+    <p><strong>Name:</strong> ${data.name}</p
+    ><p><strong>Bio:</strong> ${data.bio}</p>
+    <p><strong>Location:</strong> ${data.location}</p>
+    <p><strong>Number of public repos:</strong> ${data.public_repos}</p>
+    </div>`;    
+// 3f. Displays profile overview section
+    profileInfoOverview.append(div);
+//Displays repos from github
+    getUserRepos();
+    //console.log(profileInfoOverview);
 };
 
 //FETCH REPO DATA
 
 // 2. Fetch Your Repos
 // 2a. Create async function that fetches repos
-const getUserRepo = async function () {
+const getUserRepos = async function () {
 // 2b. Fetch Repo using API and paramters: sort by updated, show 100 repos per page
     const fetchRepos = await fetch(`https://api.github.com/users/${username}/repos?sort=updated&per_page=100`);
     const repoData = await fetchRepos.json();
 // 6. Save and View Your Page
     displayRepos(repoData);
 };
-//getUserRepo();
 
 // 5. Display Info About Your Repos
 // 5a. Create function that displays each repos info
@@ -75,4 +79,68 @@ const displayRepos = function (repos) {
 // 5e. Append repo item so it shows in repo list        
         repoList.append(repoItem);
     }    
+};
+
+// DISPLAY REPO INFO
+
+// 7. Declare Two New Global Variables
+    //At the top of code
+
+    
+// 8. Add a Click Event
+// 8a. Event listener for repoList
+repoList.addEventListener('click', function (e) {
+// 8b. Check 'if' event target that was clicked matches the <h3> element
+    if (e.target.matches('h3')) {
+        const repoName = e.target.innerText;
+        //console.log(repoName);
+// 9b. Displays specific repo info
+        getRepoInfo(repoName);        
+    }      
+});
+
+// 9. Create a Function to Get Specific Repo Info
+const getRepoInfo = async function (repoName) {
+// 9a.Fetch info about specific repo
+    const fetchInfo = await fetch(`https://api.github.com/repos/${username}/${repoName}`);
+    const repoInfo = await fetchInfo.json();
+    console.log(repoInfo);
+// 10. Create an Array of Languages
+// 10a. Create variable that fetches data from the language_url property of repoInfo
+    const fetchLanguages = await fetch(repoInfo.languages_url);
+// 10b. Variable to hold json parse
+    const languageData = await fetchLanguages.json();
+    //console.log(languageData);
+// 10c. Create an empty array to list languages
+    const languages = [];
+// 10d. Loop through the languageData object 'for' languages
+    for (const language in languageData) {
+// 10e. Add language to the end of the array
+        languages.push(language);
+    }        
+    //console.log(languages);
+// 12. Call Your Function & View Your Work
+    displayRepoInfo(repoInfo, languages);
+};
+
+
+
+// 11. Create a Function to Display Specific Repo Info
+const displayRepoInfo = function (repoInfo, languages) {
+// 11a. Empty repo-data innerHTML    
+    reposDataArea.innerHTML = '';
+// 11b. Create div element with repo: name, description, default branch and github link
+    const div = document.createElement('div');
+    div.innerHTML = `
+    <h3>Name: ${repoInfo.name}</h3>
+    <p>Description: ${repoInfo.description}</p>
+    <p>Default Branch: ${repoInfo.default_branch}</p>
+    <p>Languages: ${languages.join(", ")}</p>
+    <a class="visit" href="${repoInfo.html_url}" target="_blank" rel="noreferrer noopener">View Repo on GitHub!</a>`;
+// 11c. Append div with class of 'repo-data'
+    reposDataArea.append(div);
+// 11d. Remove hide class from 'repo-data' element
+    reposDataArea.classList.remove('hide');
+// 11e. Hide element with class of 'repos'    
+    reposArea.classList.add('hide');
 };
